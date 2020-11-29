ba11775accfdd8b2feb906546d9b13ee0762bc41
Vidheesh Kumar Nacode <vid@hedwig.local>
2020-11-29 16:23:41 +0530

    till async & routing.

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from 'react-dom'
+import { Router, Link } from '@reach/router';
+//import  Pet  from './Pet';
+import SearchParams from "./SearchParams";
+import Details from './Details';
+
+const App = () => {
+
+  return  ( 
+  <div>
+    <header>
+      <Link to = "/">Adopt Me!</Link>
+      </header>
+      <Router>
+      <SearchParams path = "/" />
+      <Details path="/details/:id" />
+      </Router>
+      
+      
+    </div>
+    
+
+  );
+};
+render(<App/>, document.getElementById("root"));
---
+++ src/Details.js
@@ -0,0 +1,33 @@
+import React from "react";
+import pet from '@frontendmasters/pet';
+
+class Details extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            loading : true
+        };
+    }
+
+    componentDidMount(){
+        pet.animal(this.props.id)
+        .then(({animal}) => {
+            this.setState({
+                name: animal.name,
+                animal : animal.type,
+                location: `${animal.contact.address.city}, 
+                ${animal.contact.address.state}`,
+                description : animal.description,
+                media: animal.photos,
+                breed: animal.breeds.primary,
+                loading: false
+            })
+        })
+    }
+    render(){
+
+    }
+}
+
+export default Details;
